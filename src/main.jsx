ee28598ed3d4529a0fbe603c89dd8f9ecbea5b2a
kelliaUmuhire <kellumuhire@gmail.com>
2024-07-01 05:23:09 +0200

    fix: remove unused imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <DndProvider backend={HTML5Backend}>
-        <App />
-      </DndProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
